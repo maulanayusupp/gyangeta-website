b1af79fdfd66e8f1e8e5d29a740524fa6a7a9433
Maulana <maulanayusupp@gmail.com>
2024-09-28 14:15:53 +0700

    first commit

---
+++ layouts/custom.vue
@@ -0,0 +1,41 @@
+<template>
+	<Html>
+			<!-- Head -->
+			<Head>
+					<Title>Gyan Geta Nusantara - Inovasi Kecantikan</Title>
+					<Meta name="description" :content="`Gyan Geta Nusantara - Inovasi Kecantikan`" />
+					<!-- <Link rel="preload" href="/test.txt" as="script" /> -->
+					<!-- <Style type="text/css" :children="styleString" /> -->
+			</Head>
+
+			<!-- Header -->
+			<nav-header />
+
+			<!-- Content -->
+			<slot />
+
+			<!-- Footer -->
+			<!-- <the-footer /> -->
+	</Html>
+</template>
+
+<script>
+definePageMeta({
+  layout: "custom",
+});
+
+import NavHeader from '@/layouts/partials/NavHeader.vue'
+import TheFooter from '@/layouts/partials/TheFooter.vue'
+
+export default {
+	components: {
+		NavHeader,
+		TheFooter,
+	},
+	setup() {
+		return {
+		}
+	},
+}
+</script>
+
